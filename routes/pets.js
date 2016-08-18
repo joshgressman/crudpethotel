fd99fe13927d1be7f01b43f059899bb5e4f18459
nah <2trill2spill@gmail.com>
2016-08-18 15:00:21 -0500

    Create pets GET route.

--- routes/pets.js
+++ routes/pets.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var pg = require('pg');
+var connectionString = 'postgres://localhost:5432/petHotel'
 
 router.get('/', function(req, res) {
+  console.log("GET /pets/");
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log("Database connction failed");
+      res.sendStatus(500);
+    }
 
+    client.query('SELECT * FROM pets', function(err, result) {
+      done();
+
+      if(err) {
+        console.log("Query failed: ", err);
+        res.sendStatus(500);
+      }
+      console.log("result: ", result.rows);
+      res.send(result.rows);
+    })
+  })
 });
 
 module.exports = router;
