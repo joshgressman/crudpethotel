e1db21782b48931db2aed2d2e268da2695bf5872
nah <2trill2spill@gmail.com>
2016-08-18 15:23:03 -0500

    Add post route for owners.

--- routes/owners.js
+++ routes/owners.js
@@ -24,4 +24,29 @@ router.get('/', function(req, res) {
   })
 });
 
+router.post('/', function(req, res) {
+  var owner = req.body;
+
+  pg.connect(connectionString, function(err, client, done) {
+    if(err) {
+      console.log("Database connction failed");
+      res.sendStatus(500);
+    }
+
+    client.query('INSERT INTO owners (first_name, last_name)'
+                 + 'VALUES($1, $2)',
+                 [owner.firstName, owner.lastName],
+                  function(err, result) {
+      done();
+
+      if(err) {
+        console.log("Query failed: ", err);
+        res.sendStatus(500);
+      }
+      console.log("result: ", result.rows);
+      res.send(result.rows);
+    })
+  })
+});
+
 module.exports = router;
